a6c5133312b2b16cc6da8dc9771fe0941d93b87a
gyok <gyok@ya.ru>
2017-03-02 13:27:28 +0200

    creat first script for creating tree of directories and directory for one time using scripts

---
+++ removeAfterUse/creatTreeDirectory.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+mkdir -p ~/download/torrent
+mkdir -p program/{build,project}
+mkdir -p media/audio
+mkdir -p media/audio/{music,audiobook,lection,melody}
+mkdir -p media/video/{movie,lection,camera}
+mkdir -p media/image/{screenshot,photo,background,enouther}
+mkdir -p document/{document,book,yandexDisk,enouther}
